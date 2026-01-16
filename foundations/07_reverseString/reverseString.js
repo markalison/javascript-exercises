4a2923e499ff5ecf4d8babbd86acf84472a689a9
markalison <markalison91@gmail.com>
2026-01-16 17:47:41 -0600

    07

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let reversedString = [];
+    let string2 = string.split("");
+    let len = string2.length;
+    for (let i = 1; i <= len; i++) {
+        reversedString.push(string2.at(-i));
+    }
+    return reversedString.join("");
 };
+
+console.log(reverseString("asc"))
 
 // Do not edit below this line
 module.exports = reverseString;
